394ac225d9afb5ee2b9ae499ef1bd557369d34e4
Vox Bardot <voxbardot@protonmail.com>
2022-02-22 21:31:51 +1000

    fix: _app.tsx types

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,10 +1,10 @@
 import { AppProps } from "next/app"
 import { ChakraProvider } from "@chakra-ui/react"
 
-export default function App({ Component, PageProps }) {
+export default function App({ Component, pageProps }: AppProps) {
     return (
         <ChakraProvider>
-            <Component {...PageProps} />
+            <Component {...pageProps} />
         </ChakraProvider>
     )
 }
